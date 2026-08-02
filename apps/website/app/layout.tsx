b808bddd80205269644e13f4dc465db47fc7b147
mdwt <50661574+mdwt@users.noreply.github.com>
2026-08-02 14:30:52 +0200

    refactor: named import

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react';
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
@@ -72,7 +73,7 @@ export const viewport: Viewport = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html
